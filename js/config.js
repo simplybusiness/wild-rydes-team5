0a0a9d7c98e67861ff2c2ceea86d2e987d925e60
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2020-10-20 14:33:42 +0100

    Add new user pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_CRWiJPdgW',
-        userPoolClientId: '2v2crmqjiejlkfn82qq9c3sbgk',
+        userPoolId: 'eu-west-2_okCtEJOEa',
+        userPoolClientId: '4v67d3qqvn979kpcutgrvddllm',
         region: 'eu-west-2'
     },
     api: {

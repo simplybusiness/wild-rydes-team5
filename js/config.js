77d782933eeca8e80ab0cff9cea0c99da6284322
John Gallagher <john.gallagher@simplybusiness.co.uk>
2020-10-20 11:45:30 +0100

    Fill in credentials for existing user pool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'wildrydes-team-a-dev',
+        userPoolClientId: '3qitcsk0qvfn4g27t5ciblns7v',
+        region: 'eu-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
